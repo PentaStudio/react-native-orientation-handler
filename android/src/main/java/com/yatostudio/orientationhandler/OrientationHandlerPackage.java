22b2bb017ac45ddd8ce00b77a7b7db79ef864cee
lmeireles <luis.lc93@hotmail.com>
2019-09-12 18:29:28 -0300

    FIX - Java ArrayList missing import

--- android/src/main/java/com/yatostudio/orientationhandler/OrientationHandlerPackage.java
+++ android/src/main/java/com/yatostudio/orientationhandler/OrientationHandlerPackage.java
@@ -6,7 +6,7 @@ import com.facebook.react.bridge.NativeModule;
 import com.facebook.react.bridge.ReactApplicationContext;
 import com.facebook.react.uimanager.ViewManager;
 
-import java.util.Arrays;
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
@@ -15,19 +15,18 @@ public class OrientationHandlerPackage implements ReactPackage {
     @Override
     public List<NativeModule> createNativeModules(ReactApplicationContext reactContext) {
         List<NativeModule> modules = new ArrayList<>();
-
         modules.add(new OrientationHandlerModule(reactContext));
-
         return modules;
     }
 
+    // Deprecated RN 0.47
     public List<Class<? extends JavaScriptModule>> createJSModules() {
         return Collections.emptyList();
     }
 
     @Override
     public List<ViewManager> createViewManagers(ReactApplicationContext reactContext) {
-        return Arrays.asList();
+        return Collections.emptyList();
     }
 }
 
